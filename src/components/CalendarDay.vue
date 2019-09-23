e6ad7de6a80967ee6504e1b8e65b86b0b69fd3bd
llabati <33926231+llabati@users.noreply.github.com>
2019-09-23 17:15:17 +0200

    Installation des fonctions de base

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,5 +1,10 @@
 <template>
-<div :class="classObject">{{ day.format('D') }}</div>
+<div :class="classObject" @click="openEvent">{{ day.format('D') }}
+    <ul class="event-list">
+        <li v-for="event in events" :key="event.id">{{ event.description }}</li>
+    </ul>
+
+</div>
     
 </template>
 <script>
@@ -7,12 +12,25 @@ export default {
     props: [ 'day' ],
     computed: {
         classObject(){
+            let eventDate = this.$store.state.eventDate
+            let eventActive = this.$store.state.active
             let today = this.day.isSame(this.$moment(), 'day')
             return {
                 day: true,
                 today,
-                past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
+                past: this.day.isSameOrBefore(this.$moment(), 'day') && !today,
+                active: eventDate === this.day._d && eventActive
             }
+        },
+        events(){
+            return this.$store.state.events.filter(e => e.date === this.day._d,)
+        }
+    },
+    methods: {
+        openEvent(event) {
+            this.$store.commit('SET_EVENT_POS', event)
+            this.$store.commit('OPEN_EVENT')
+            this.$store.commit('ATTRIBUTE_DATE', this.day._d)
         }
     }
 }
