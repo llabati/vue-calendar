7df8387a7a6fdbff7bd960ecf4ccdd3e8d0d1bdf
llabati <33926231+llabati@users.noreply.github.com>
2019-09-23 17:24:01 +0200

    Installation des fonctions de base

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <!--<div>{{ formattedDate | capitalize() }}</div>-->
+        <div>{{ formattedDate | capitalize() }}</div>
         <button ref="past" @click="toPast">-</button>
         <button ref="future" @click="toFuture">+</button>
     </div>
 </template>
 <script>
 import { store } from '../store/index.js'
+
 export default {
+    
     store,
     computed: {
         year(){
@@ -17,29 +19,27 @@ export default {
           console.log(this.$store.state.month)
           return this.$store.state.month
       },
-      formattedDate(event){
-        let time = new Date()
-        let options = { year: 'numeric', month: 'long' }
-        time.toLocaleDateString('fr-FR', options)
-        /*if (!event){
-             return this.$moment().format('MMMM YYYY') 
-          }
-        if (event.target === this.$refs.past) {
-            console.log('PAST')
-            return this.$moment().subtract(1, 'month').format('MMMM YYYY')
-        }
-        if (event.target === this.$refs.future) {
-            console.log('FUTURE')
-            return this.$moment().add(1, 'month').format('MMMM YYYY')
-        }*/
+      formattedDate() {
+          return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY')
+        
       }
     },
     methods: {
         toPast(){
-            this.$store.commit('SET_CURRENT_MONTH', this.month - 1)
+            if (this.month === 1){
+                this.$store.commit('SET_CURRENT_MONTH', 12)
+                this.$store.commit('SET_CURRENT_YEAR', this.year - 1)
+            }
+            else this.$store.commit('SET_CURRENT_MONTH', this.month - 1)
+            
         },
         toFuture(){
-            this.$store.commit('SET_CURRENT_MONTH', this.month + 1)
+            if (this.month === 12){
+                this.$store.commit('SET_CURRENT_MONTH', 1)
+                this.$store.commit('SET_CURRENT_YEAR', this.year + 1)
+            }
+            else this.$store.commit('SET_CURRENT_MONTH', this.month + 1)
+            
         }
     },
     filters: {
@@ -49,9 +49,7 @@ export default {
             return `${capital}${suit}`
         }
     },
-    mounted(){
-        console.log(this.$moment.locale())
-    }
+    
 }
 </script>
 <style>
