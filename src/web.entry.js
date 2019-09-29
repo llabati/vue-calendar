45a4174e27040bce6cbe086172dd38446b259ac8
llabati <33926231+llabati@users.noreply.github.com>
2019-09-29 17:57:21 +0200

    Persistance des données et rendu SSR

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import './style.scss'
-
 import moment from 'moment-timezone'
 moment.tz.setDefault('Europe/Paris')
-moment.locale('fr')
-moment.locale('en', null)
-Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } } )
-
+import store from './store/index.js'
 import App from './components/App.vue'
 
+
+import VueCalendar from './entry'
+VueCalendar(store.state.events).$mount('#app')
+//VueCalendar(store.state.events).$mount('#app')
 new Vue({
   el: '#app',
   data: {
     moment
   },
+  store,
+  render(createElement){
+    return createElement(
+      'div',
+      { attrs: { id: 'app'}},
+      [ createElement('app')]
+    )
+  },
   
   components: {
     App
   },
+  mounted(){
+    this.$store.commit('RETRIEVE_ALL_EVENTS')
+  }
   
 });
